b1090704b8811224777302cc195e24a5fe01bcb8
dipsywong98 <ycwongal@connect.ust.hk>
2020-07-10 18:32:29 +0800

    Updates

---
+++ precache-manifest.874696f7109ec2b0cf9765996dd90261.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "950af5372753dccd2887",
+    "url": "/static/js/app.c56756bb.chunk.js"
+  },
+  {
+    "revision": "77cafc906f42b4a876d5",
+    "url": "/static/js/runtime~app.0dd01ab9.js"
+  },
+  {
+    "revision": "9502095c4f22cda18e45",
+    "url": "/static/js/2.0c3373a2.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "8503789c9ec64e61d06ffb48eedeefc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748105f2656ac56c126e901ee60fd6e2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
